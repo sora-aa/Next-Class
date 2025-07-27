d0a2c7ac9cd4b8d28525dc87b1d9189084a9ee20
sora <sor4.cielo@gmail.com>
2025-07-27 18:52:13 +0900

    次の授業のデータを表示する機能の追加

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const now = new Date();
-console.log(now);
 
-const dayOfWeek = now.getDay();
+let dayOfWeek = now.getDay();
 const dayNames = ["日", "月", "火", "水", "木", "金", "土"];
-const day = dayNames[dayOfWeek];
+let day = dayNames[dayOfWeek];
 const month = now.getMonth() + 1;
 const date = now.getDate();
 const hour = now.getHours();
 const minutes = now.getMinutes();
 
 const scheduleData = [
-    { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null },
+    { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null },
     {
         1: {
             classname: "リスニング・スピーキング演習Ⅰ",
@@ -24,9 +23,8 @@ const scheduleData = [
         },
         5: null,
         6: null,
-        7: null,
     },
-    { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null },
+    { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null },
     {
         1: null,
         2: {
@@ -43,7 +41,6 @@ const scheduleData = [
             classname: "ミクロ経済学",
             classroom: "オンデマンド",
         },
-        7: null,
     },
     {
         1: {
@@ -61,7 +58,6 @@ const scheduleData = [
             classroom: "2317",
         },
         6: null,
-        7: null,
     },
     {
         1: null,
@@ -76,12 +72,17 @@ const scheduleData = [
             classroom: "3106",
         },
         6: null,
-        7: null,
     },
-    { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null },
+    { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null },
 ];
-
-console.log(scheduleData[1][1].classname);
+const classTimes = [
+    "9:00 ~ 10:30",
+    "10:40 ~ 12:10",
+    "13:00 ~ 14:30",
+    "14:45 ~ 16:15",
+    "16:30 ~ 18:00",
+    "18:15 ~ 19:45",
+];
 
 const datetime = document.querySelector("#datetime");
 const className = document.querySelector("#class-name");
@@ -89,3 +90,36 @@ const classTime = document.querySelector("#class-time");
 const classroom = document.querySelector("#classroom");
 
 datetime.textContent = `${month}/${date} (${day}) ${hour}:${minutes}`;
+
+const elapsedMinutes = hour * 60 + minutes;
+let classPeriod;
+if (elapsedMinutes < 630) {
+    classPeriod = 1;
+} else if (elapsedMinutes < 730) {
+    classPeriod = 2;
+} else if (elapsedMinutes < 870) {
+    classPeriod = 3;
+} else if (elapsedMinutes < 975) {
+    classPeriod = 4;
+} else if (elapsedMinutes < 1080) {
+    classPeriod = 5;
+} else if (elapsedMinutes < 1185) {
+    classPeriod = 6;
+} else {
+    classPeriod = 1;
+}
+
+while (scheduleData[dayOfWeek][classPeriod] === null) {
+    classPeriod++;
+    if (classPeriod === 7) {
+        classPeriod = 1;
+        dayOfWeek++;
+        if (dayOfWeek === 7) {
+            dayOfWeek = 0;
+        }
+    }
+}
+
+className.textContent = scheduleData[dayOfWeek][classPeriod].classname;
+classroom.textContent = scheduleData[dayOfWeek][classPeriod].classroom;
+classTime.textContent = classTimes[classPeriod - 1];
